5f81946ce2489c1719ddb065cad8f327917a0b94
Sreekara <syachama@purdue.edu>
2019-01-13 09:56:01 -0500

    Initial Commit

---
+++ Tip Calculator/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Tip Calculator
+//
+//  Created by Sreekara Yachamaneni on 1/13/19.
+//  Copyright © 2019 Sreekara Yachamaneni. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
